463d5825e8893335ff31f2b0add069c5fdbb5282
Eric Greene <eric@t4d.io>
2021-03-01 16:45:28 -0500

    add small tweaks to exercise 3

--- colors.py
+++ colors.py
@@ -1,47 +1,41 @@
+next_color_id = 0
+
+def add_color(colors, name, hexcode):
+
+    global next_color_id
+
+    next_color_id = next_color_id + 1
+    colors.append((next_color_id, name, hexcode))
+
+def remove_color_by_id(colors, color_id):
+
+    for color in colors:
+        if color[0] == color_id:
+            colors.remove(color)
+            break
+
+def color_table(colors):
+
+    table = []
+
+    table.append("Id     Name     Hexcode")
+    table.append("-----------------------")
+
+    if len(colors) < 1:
+        table.append("There are no colors")
+    else:
+        for id, name, hexcode in colors:
+            table.append(f"{str(id).rjust(2)} {name.ljust(12)} {hexcode}")
+
+    return "\n".join(table)
+
 
 colors = []
 
-colors.append((1, "red", "#ff0000"))
-colors.append((2, "green", "#00ff00"))
-colors.append((3, "blue", "#0000ff"))
+add_color(colors, "red", "#ff0000")
+add_color(colors, "green", "#00ff00")
+add_color(colors, "blue", "#0000ff")
 
-print("Id     Name     Hexcode")
-print("-----------------------")
-
-if len(colors) < 1:
-    print("There are no colors")
-else:
-    for id, name, hexcode in colors:
-        print(f"{str(id).rjust(2)} {name.ljust(12)} {hexcode}")
-
-color_id = 2
-
-# color_index = 0
-# not_found = True
-
-# while not_found and color_index < len(colors):
-    
-#     if colors[color_index][0] == color_id:
-#         colors.remove(colors[color_index])
-#         not_found = False
-
-#     color_index = color_index + 1
-
-for color in colors:
-    if color[0] == color_id:
-        colors.remove(color)
-        break
-
-
-print(colors)
-
-
-# print(color)
-# print(color[0]) # id
-# print(color[1]) # name
-
-# color_id, color_name, color_hexcode = color
-
-# print(color_id)
-# print(color_data)
-# print(color_hexcode)
+print(color_table(colors) + "\n")
+remove_color_by_id(colors, 2)
+print(color_table(colors))
--- exercises/exercise-3.md
+++ exercises/exercise-3.md
@@ -1,9 +1,9 @@
 # Exercise 3
 
-1. Remove the result variable.
+1. Refactor repetitive code into functions.
 
-2. Refactor repetitive code into functions.
+2. Remove the result variable.
 
 3. Write a function which calculates the result using the history. The function should have no print statements in it. Call the function from within the command while loop to get the result and display it.
 
-4. Upgrade the history command to display the number of adds, subtracts, multiplies, and divides in this history. Display this below the history table when the history command is executed. Apply the various languages feature learned to make the code as well organized as possible.
+4. Upgrade the history command to display the number of adds, subtracts, multiplies, and divides in the history. Display this below the history table when the history command is executed. Apply the various language features learned to make the code as well organized as possible.
