e293b6b75b1662b0cd90e60209e9c96e27dca370
Eric Greene <eric@t4d.io>
2021-03-01 13:20:54 -0500

    add exercise 1 solution

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-
-# first_name = input("What is your first name?")
-# age = int(input("What is your age?"))
-
-# # #print("First Name: " + first_name.upper())
-# print(f"First Name: {first_name}")
-# # print("Age: " + str(age))
-# print(f"Age: {age}")
-
-# if age < 65:
-#     print("You do not qualify for Medicare")
-# elif age >= 85: 
-#     print("You do not qualify for Super Medicare")
-# else:
-#     print("You do qualify for Medicare")
-
-g = True # False
+result = 0
 
 command = input("Please enter a command: ")
 print(command)
 
 while command:
-    
+
+    if command == "clear":
+        result = 0
+    else:
+        num = int(input("Please enter an operand: "))
+
+        if command == "add":
+            result = result + num
+        elif command == "subtract":
+            result = result - num
+        elif command == "multiply":
+            result = result * num
+        elif command == "divide":
+            result = result / num
+
+        print(f"Result: {result}")
+
     command = input("Please enter a command: ")
     print(command)
-    print(type(command))
-
-
