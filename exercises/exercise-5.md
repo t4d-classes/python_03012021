bc3f9b7bca66e520eef6f404539d5b1ead9e8653
Eric Greene <eric@t4d.io>
2021-03-02 16:55:00 -0500

    add exercise 5

---
+++ exercises/exercise-5.md
@@ -0,0 +1,8 @@
+# Exercise 5
+
+1. Upgrade the calculator to use a virtual environment. Create and activate the virtual environment. Update Visual Studio Code to use the new environment.
+
+2. Organize the functions into at least two modules (more can used). The functions from the modules will be imported into the main programming file `calc.py`
+
+3. Update `calc.py` to run as a module. Configuration Visual Studio Code to run/debug `calc.py` as a module.
+
--- transforms.py
+++ transforms.py
@@ -6,28 +6,27 @@ nums = [1,2,3,4,5]
 def double(num):
     return num * 2
 
-# def transform(transform_fn, items):
-#     new_items = []
+def transform(transform_fn, items):
 
-#     for item in items:
-#         new_items.append(transform_fn(item))
+    for item in items:
+        print("transform item")
+        yield transform_fn(item)
     
-#     return new_items
 
 # double_nums = []
 
 # for num in nums:
 #     double_nums.append(double(num))
 
-# double_nums = transform(double, nums)
+double_nums = transform(double, nums)
 # double_nums = list(map(double, nums))
 # double_nums = list(map(lambda n: n * 2, nums))
 
-# for double_num in map(double, nums):
-#     print(double_num)
-#     if double_num > 4: break
+for double_num in double_nums:
+    print(double_num)
+    if double_num > 4: break
 
-double_nums = [ n * 2 for n in nums ]
+# double_nums = [ n * 2 for n in nums ]
 
 print(nums)
-print(double_nums)
+print(list(double_nums))
