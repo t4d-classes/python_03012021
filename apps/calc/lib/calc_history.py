ef91a3ca8f51075cc34a557f9cb546faac3a341d
Eric Greene <eric@t4d.io>
2021-03-04 09:20:57 -0500

    add exercise 9 solution

--- apps/calc/lib/calc_history.py
+++ apps/calc/lib/calc_history.py
@@ -5,14 +5,25 @@ class CalcHistoryEntry:
         self.op_name = op_name
         self.op_value = op_value
 
-def append_calc_history(calc_history_list, op_name, op_value):
-    next_calc_history_id = max( [ t.id for t in calc_history_list ] or [0] ) + 1
-    new_calc_history_entry = CalcHistoryEntry(next_calc_history_id, op_name, op_value)
-    calc_history_list.append(new_calc_history_entry)
+class CalcHistoryList:
 
+    def __init__(self):
+        self._calc_history = []
 
-def remove_calc_history_entry(calc_history_list, calc_history_entry_id):
-    for calc_history_entry in calc_history_list:
-        if calc_history_entry.id == calc_history_entry_id:
-            calc_history_list.remove(calc_history_entry)
-            break
+    @property
+    def calc_history(self):
+        return self._calc_history
+
+    def append_calc_history(self, op_name, op_value):
+        next_calc_history_id = max( [ t.id for t in self._calc_history ] or [0] ) + 1
+        new_calc_history_entry = CalcHistoryEntry(next_calc_history_id, op_name, op_value)
+        self._calc_history.append(new_calc_history_entry)
+
+    def remove_calc_history_entry(self, calc_history_entry_id):
+        for calc_history_entry in self._calc_history:
+            if calc_history_entry.id == calc_history_entry_id:
+                self._calc_history.remove(calc_history_entry)
+                break
+    
+    def clear_history(self):
+        self._calc_history.clear()
